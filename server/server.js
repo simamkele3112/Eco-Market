cbb70d91f59a99557bbb3690628a2d11bedaf07c
lwandile9 <lwandiletoto9gmail.com>
2025-01-20 11:04:54 +0200

    fixed sever 2025-01

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 
 mongoose
-	.connect("mongodb://localhost:27017/marketplace", {
+	.connect("mongodb://localhost:2701/marketplace", {  // this main route      
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	})
